79d4984246a16692d07ca1dae004929f83351821
mglunafh <ufispb@gmail.com>
2012-10-03 18:18:35 +0400

    'find' method is reimplemented, a lot of useless methods were deleted, 'delete' is still undone :/

--- 2012.09.10/SearchTree/src/searchtree/SearchTree.java
+++ 2012.09.10/SearchTree/src/searchtree/SearchTree.java
@@ -43,138 +43,44 @@ public class SearchTree {
      * @return 'true', if element was found, and 'false' otherwise.
      */
     public boolean find(int value) {
+        return (null != this.findPosition(value));
+    }
+
+    private TreeElement findPosition(int value) {
         if (0 != this.count) {
-            //We may be in luck and the value to be found may lie in the top.
-            if (value == this.top.value) {
-                return true;
-                //In general case we'll use function 'findParentalPosition'.
-            } else {
-                return null == this.getCorrectSon(this.findParentalPosition(value), value);
+            TreeElement temp = this.top;
+            while (value != temp.value || null != temp) {
+                if (value < temp.value) {
+                    temp = temp.leftSon;
+                } else {
+                    temp = temp.rightSon;
+                }
             }
-            //There should not be questions, when the tree is empty.
+            return temp;
         } else {
-            return false;
+            return null;
         }
     }
 
     /*
-     * public void delete(int value) {
-        if (0 == count) {
-            System.out.println("Tree is empty NAPRIMER.");
-        } else {
-            TreeElement parent = this.findParentalPosition(value);
-
-            // this branch is not ready yet.
-            if (value == parent.value) {
-            } else {
-                TreeElement deleted = this.getCorrectSon(parent, value);
-                if (null == deleted) {
-                    System.out.println("Given value does not exist.");
-                } else {
-                    TreeElement theClosestLeft = this.theClosestLeftSon(parent);
-                    TreeElement theClosestRight = this.theClosestRightSon(parent);
-
-                    if (null != theClosestRight) {
-                        if (null == theClosestRight.leftSon) {
-                            deletedElement.value = theClosestRight.value;
-                            deletedElement.rightSon = null;
-                        } else {
-                            while (null != theClosestRight.leftSon) {
-                                theClosestRight = theClosestRight.leftSon;
-                            }
-                            deletedElement.value = theClosestRight.value;
-                        }
-                    } else if (null != theClosestLeft) {
-                    } else {
-                    }
-                }
-            }
-        }
-    }
-    *
-    */
-
-    
-    /**
-     * Finds a parent of a given value.
+     * public void delete(int value) { if (0 == count) {
+     * System.out.println("Tree is empty NAPRIMER."); } else { TreeElement
+     * parent = this.findParentalPosition(value);
      *
-     * @param value
-     * @return top in case, when value is in the top, the closest un-null
-     * position, if there is not the given value, and the true parent otherwise.
-     */
-    private TreeElement findParentalPosition(int value) {
-        TreeElement result = this.top;
-        if (value == result.value) {
-            return result;
-        }
-        TreeElement temp = null;
-        if (value < result.value) {
-            temp = result.leftSon;
-        } else {
-            temp = result.rightSon;
-        }
-
-        while (temp != null || temp.value != value) {
-            if (temp.value > value) {
-                result = temp;
-                temp = temp.leftSon;
-            } else if (temp.value < value) {
-                result = temp;
-                temp = temp.rightSon;
-            }
-        }
-        return result;
-    }
-
-    /**
+     * // this branch is not ready yet. if (value == parent.value) { } else {
+     * TreeElement deleted = this.getCorrectSon(parent, value); if (null ==
+     * deleted) { System.out.println("Given value does not exist."); } else {
+     * TreeElement theClosestLeft = this.theClosestLeftSon(parent); TreeElement
+     * theClosestRight = this.theClosestRightSon(parent);
      *
-     * @param treeElement "Parent" of the tree element, containing 'value'.
-     * @param value
-     * @return null, if 'value' does not belong to the tree, or its position in
-     * the structure.
-     */
-    private TreeElement getCorrectSon(TreeElement treeElement, int value) {
-        if (value > treeElement.value) {
-            return treeElement.rightSon;
-        } else {
-            return treeElement.leftSon;
-        }
-    }
-
-    /**
-     * Finds the least number, which is greater than value in 'element'.
+     * if (null != theClosestRight) { if (null == theClosestRight.leftSon) {
+     * deletedElement.value = theClosestRight.value; deletedElement.rightSon =
+     * null; } else { while (null != theClosestRight.leftSon) { theClosestRight
+     * = theClosestRight.leftSon; } deletedElement.value =
+     * theClosestRight.value; } } else if (null != theClosestLeft) { } else { }
+     * } } } }
      *
-     * @param element
-     * @return
      */
-    private TreeElement theClosestRightSon(TreeElement element) {
-        TreeElement temp = element.rightSon;
-        if (temp == null) {
-            return temp;
-        }
-        while (temp.leftSon != null) {
-            temp = temp.leftSon;
-        }
-        return temp;
-    }
-
-    /**
-     * Finds the greatest number, which is less than than value in 'element'.
-     *
-     * @param element
-     * @return
-     */
-    private TreeElement theClosestLeftSon(TreeElement element) {
-        TreeElement temp = element.leftSon;
-        if (temp == null) {
-            return temp;
-        }
-        while (temp.rightSon != null) {
-            temp = temp.rightSon;
-        }
-        return temp;
-    }
-
     /**
      * Class which corresponds to element of the tree.
      */
@@ -210,7 +116,6 @@ public class SearchTree {
             } else {
             }
         }
-        
         private int value;
         private TreeElement batya;
         private TreeElement leftSon;
